64f7d97c5c82d2567a4fe8b9e502fe8c7da36536
Sebastian <Fancy.Sebastian@cbre.com>
2018-08-20 15:17:38 +0530

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    siteMetadata: {
+      title: 'Gatsby Default Starter',
+    },
+    plugins: [
+      'gatsby-plugin-react-helmet',
+      'gatsby-plugin-netlify-cms',
+      'gatsby-plugin-sass',
+      'gatsby-plugin-stylus',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options:{
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      }
+    },
+    {
+    resolve:'gatsby-transformer-remark',
+    options: {
+      plugins: [] 
+    },
+  },
+  ],
+  }
+  
